f0e12c40b523eda8dac0295d6d7528b933267e2d
swcodingschool <swcodingschool@gmail.com>
2021-11-30 16:10:49 +0900

    2021-11-30 16:10

---
+++ js/operator.js
@@ -0,0 +1,85 @@
+// 1. String concatenation
+console.log('my' + ' dog');
+console.log('1' + 2);
+console.log(`string literals : 1 + 2 = ${1 + 2}` );
+
+// 2. Numeric Operators
+console.log(1 + 1); // add
+console.log(1 - 1); // substract
+console.log(1 / 1); // divide
+console.log(1 * 1); // multiply
+console.log(5 % 2); // remainder
+console.log(2 ** 3); // exponentiation
+
+// 3. increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter:${counter}`);
+counter = 2;
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter:${counter}`);
+
+// 4. Assignment operators
+let x = 3;
+let y = 6;
+x += y; // x = x + y;
+x -= y;
+x *= y;
+x /= y;
+
+// 5. comparison operators
+
+// 6. logincal operators : || (or), && (and), !(not)
+const value1 = false;
+const value2 = 4 < 2;
+// || (or)
+console.log(`or : ${value1 || value2 || check()}`);  // value1 이 true 인 경우 뒤는 더이상 검토하지 않음... 함수에서 출력하는 룰루랄라가 출력되지 않을 수 있음....
+
+function check() {
+  for (let i = 0; i < 10; i++) {
+    //wasting time
+    console.log('♬');
+  }
+  return true;
+}
+
+
+// 7. equality
+const stringFive = '5';
+const numberFive = 5;
+// == loose equality, with type conversion
+console.log(stringFive == numberFive);
+console.log(stringFive != numberFive);
+// === strict equality, no type conversion
+console.log(stringFive === numberFive);
+console.log(stringFive !== numberFive);
+
+
+// 8. conditional operators : if
+// if, else if, else
+const name = 'john'
+if(name === 'john') {
+  console.log('Welcome, John');
+}else if(name ==='coder'){
+  console.log('you are amazing coder');
+}else {
+  console.log('unknown');
+}
+
+// 9. Ternary operator: ?
+// condition ? vlaue1 : value2;
+console.log(name === 'john' ? 'yes' : 'no');
+
+// 10. switch statement
+// switch () {   case : break; ...   default: break;}
+
+//11. Loops
+// while loop, whiel the condition is truthy, body code is executed
+// while() { }
+// do { } while(조건비교);
+// for loop, for(begin;condition;step) { }
+// nested loops
--- js/variable.js
+++ js/variable.js
@@ -1,6 +1,7 @@
 // 1. use strict mode for valid Javascript
 'use strict';
-// 2. variable
+
+// 2. variable, rw(read/write) : 변수는 값을 읽거나 쓰는 것이 가능... 변경 가능
 // let (added in ES6)
 let globalName = 'global name';
 {
@@ -24,7 +25,7 @@ age = 4;
 var age;
 
 // var는 block scope가 없다.
-//3. constants  : 
+//3. constant  :   read only
 // favor immutable data type always for a few reasons:
 // - security
 // - thread safety
@@ -35,3 +36,73 @@ var age;
 // primitive, single type : number, string, boolean, null, undefined, symbol
 // object type : object, box container
 // function, first-class function
+
+
+const count = 100; // integer
+const size = 1.23; // decimal number
+console.log(`value: ${count}, type: ${typeof count}`)   //  grave 사용에 주의!!
+console.log(`value: ${size}, type: ${typeof size}`)
+
+// number - special numeric values : infinity, -infinity, NaN
+const infinity = 1 / 0;
+const negativeInfinity = -1 / 0;
+const nAn = 'not a number' / 2;
+console.log(infinity);
+console.log(negativeInfinity);
+console.log(nAn);
+
+// bigInt(fairly new, don't use it yet)
+const bigInt = 1234567890123456789012345678901234567890n; // over (-2**53 ~ 2**53) n을 추가함으로써 bitInt 로 변경
+console.log(`value: ${bigInt}, type: ${typeof bigInt}`);
+console.log(Number.MAX_SAFE_INTEGER);
+
+// String
+const char = 'c';
+const brendan = 'brendan';
+const greeting = 'hello ' + brendan;
+console.log(`value: ${greeting}, type: ${typeof greeting}`);
+const helloBob = `hi ${brendan}!!`;  // template literals....   grave 기호 사용함에 주의!!
+console.log(`value: ${helloBob}, type: ${typeof helloBob}`);
+
+//boolean
+// false : 0, null, udefined, NaN, ''
+// true : any other value
+const canRead = true;
+const test = 3 < 1; // false
+console.log(`value: ${canRead}, type: ${typeof canRead}`)
+console.log(`value: ${test}, type: ${typeof test}`)
+
+// null 
+let nothing = null;
+console.log(`value: ${nothing}, type: ${typeof nothing}`)
+
+// undefined
+let x;  // 또는 let x = undefined;
+console.log(`value: ${x}, type: ${typeof x}`)
+
+// symbol, create unique identifiers for objects
+const symbol1 = Symbol('id');
+const symbol2 = Symbol('id');
+console.log(symbol1 == symbol2); // false
+const gSymbol1 = Symbol.for('id');
+const gSymbol2 = Symbol.for('id');
+console.log(gSymbol1 == gSymbol2); // true
+// console.log(`value: ${symbol1}, type: ${typeof symbol1}`)  // symbol값을 바로 출력하면 오류 발생 : symbol value를 stringㅇ로 변환할 수 없다는
+console.log(`value: ${symbol1.description}, type: ${typeof symbol1}`) // description을 붙여 string으로 변환 후 출력해야 함.
+
+//object, real-life object, data structure
+const john = {name: 'john', age:26};  // object 자체는 const이지만 
+john.age = 27;   // object를 구성하고 있는 요소는 값 변경 가능함
+
+// 5. Dynamic typing : dynamically typed language
+let text = 'korea';
+console.log(`value: ${text}, type: ${typeof text}`);
+console.log(text.charAt(0));  // 스트링일때는 첫글자를 가져올 수 있으나
+// console.log(text.charAt(0));  // 숫자로 변경된 후에는 불가능함
+text = 1;
+console.log(`value: ${text}, type: ${typeof text}`); // string 타입의 변수가 값에 따라 숫자형으로 변경됨
+text = '7' + 5;
+console.log(`value: ${text}, type: ${typeof text}`); // string과 숫자형의 결합은 스트링으로 변환됨
+text = '10' / '5';
+console.log(`value: ${text}, type: ${typeof text}`); // string과 string 나누기 : 숫자료 변환하여 연산 후 숫자로 보관
+// console.log(text.charAt(0));  // 숫자로 변경된 후에는 불가능함
