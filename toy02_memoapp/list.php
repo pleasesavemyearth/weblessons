3fadc707b02cf94e926ac68e7b842f8923e98497
swcodingschool <swcodingschool@gmail.com>
2021-12-30 17:35:21 +0900

    2021-12-30 17:30

---
+++ toy02_memoapp/list.php
@@ -0,0 +1,27 @@
+<!-- 
+Filename : list.php
+First Written : 2021-12-30
+Last Updated : 2021-12-30
+Code by swcodingschool
+
+Function :
+- list the memoid,  
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Toy 2nd. MemoApp </title>
+</head>
+<body>
+    <h1>list page</h1>
+    <form action="#" method="POST" id="myform">
+        <label>item : </label><input type="text" name="item1" /><br><br><br>
+        <input type="submit">
+        <button onclick="document.getElementById('myform').reset();">RESET</button>
+    </form>
+</body>
+</html>
