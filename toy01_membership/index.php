2e1a0acb849990ebb74ca3581cc3f7a0bf9dd876
swcodingschool <swcodingschool@gmail.com>
2022-01-03 09:20:22 +0900

    20212-01-03 09:20

--- toy01_membership/index.php
+++ toy01_membership/index.php
@@ -13,13 +13,14 @@
 <!--
   session 관리 목적 추가
 -->
-<?php 
-  require_once 'asysconfig.php';
-  require_once 'b_loginchk.php';
+<?php
+require_once 'asysconfig.php';
+require_once 'b_loginchk.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -27,6 +28,7 @@
   <title>Membership Management</title>
   <link rel="stylesheet" href="./b_style.css">
 </head>
+
 <body>
   <!-- Logo, Memga menu's, Introduction Video link, and Login Button -->
   <header>
@@ -42,26 +44,28 @@
       <li>navmenu1</li>
       <li>navmenu1</li>
       <?php
-        if(!$chk_login){  // 로그인 상태가 아니라면
+      if (!$chk_login) {  // 로그인 상태가 아니라면
       ?>
-      <li id='trglgnModal'>login</li>
-      <!-- login modal -->
-      <div id='lgnModal' class='modal'>
-        <span class="close">&times;</span>
-        <form action="user_loginprocess.php" method="POST" class="modal-content">
-          <label>사용자 아이디 : </label><input type="text" name="username" placeholder="사용자 아이디를 입력해주세요." required /><br>
-          <label>비밀번호 : </label><input type="password" name="passwd" placeholder="비밀번호를 입력해주세요." required /><br>
-          <br>
-          <input type="checkbox" value="yes" name="chkbox">로그인 상태 유지
-          <input type=submit value="로그인">
-        </form>
-      </div>
+        <li id='trglgnModal'>login</li>
+        <!-- login modal -->
+        <div id='lgnModal' class='modal'>
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <form action="user_loginprocess.php" method="POST" class="loginbox">
+              <label>사용자 아이디 : </label><input type="text" name="username" placeholder="사용자 아이디를 입력해주세요." required /><br>
+              <label>비밀번호 : </label><input type="password" name="passwd" placeholder="비밀번호를 입력해주세요." required /><br>
+              <br>
+              <input type="checkbox" value="yes" name="chkbox">로그인 상태 유지
+              <input type=submit value="로그인">
+            </form>
+          </div>
+        </div>
       <?php } else {
-        echo $_SESSION['username'];?>
+        echo $_SESSION['username']; ?>
         <button?><a href="user_logout.php">logout</a></button>
-      <?php
+        <?php
       }
-      ?>
+        ?>
     </ul>
   </nav>
   <!-- -->
@@ -71,4 +75,5 @@
   </footer>
   <script src='./b_login.js'></script>
 </body>
+
 </html>
